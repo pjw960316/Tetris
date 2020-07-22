b20bb1f002751c5749a6aca2598907150575daa3
pjw960316 <pjw960316@naver.com>
2020-07-22 18:52:34 +0900

    feature : 혼자서 만들어 보는 테트리스 시작
    테트리스를 강의 없이 혼자 만들어 보려합니다.

---
+++ Tetris/Assets/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Dynamic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject j_block;
+    public Transform spawn_point;
+    public int speed;
+    
+    int cnt = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Spawn();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cnt++;
+        if(cnt%600 == 0)
+        {
+            Spawn();
+        }
+    }
+
+    void Spawn()
+    {
+        Rigidbody2D rigid = GetComponent<Rigidbody2D>();
+        Instantiate(j_block, spawn_point.position, spawn_point.rotation);
+        //rigid.velocity = new Vector2(-1, -0.1f);
+    }
+}
