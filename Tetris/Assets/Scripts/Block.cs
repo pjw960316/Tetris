faae36b599abf7a8cb7cca33a3286ce3cee61968
pjw960316 <pjw960316@naver.com>
2020-07-23 18:46:56 +0900

    feature : 아직 부족하다
    확실히 혼자서도 이제 어느정도 구현이 가능합니다.
    하지만 조금 더 강의로 학습을 하고 다시 도전해야 할 것 같습니다.
    다시 슈팅게임 만들기

--- Tetris/Assets/Scripts/Block.cs
+++ Tetris/Assets/Scripts/Block.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class Block : MonoBehaviour
 {
-    // Start is called before the first frame update
+   
+  
     void Start()
     {
-        transform.position = new Vector2(0, 1);
     }
 
-    // Update is called once per frame
     void Update()
-    {
-        
+    {        
+        Rigidbody2D rigid = GetComponent<Rigidbody2D>();       
     }
+
+    //부딪히면 멈춘다 -> 이 로직은 좋지 않다.
+    //너무 예외가 많아
+    /*void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("collision");
+        Rigidbody2D rigid = GetComponent<Rigidbody2D>();
+        if (transform.position.x - collision.transform.position.x < 1)
+        {
+            Debug.Log(transform.position.x);
+            Debug.Log(collision.transform.position.x);
+            rigid.velocity = new Vector2(0, 0);
+        }
+        else
+        {
+            Debug.Log("내려가");
+        }
+    }*/
 }
--- Tetris/Assets/Scripts/GameManager.cs
+++ Tetris/Assets/Scripts/GameManager.cs
@@ -3,17 +3,21 @@ using System.Collections.Generic;
 using System.Dynamic;
 using UnityEngine;
 
+
 public class GameManager : MonoBehaviour
 {
-    public GameObject j_block;
-    public Transform spawn_point;
-    public int speed;
-    
+    public GameObject[] blocks;
+    public Transform[] spawn_point;
+    //테트리스 게임의 난이도를 결정합니다.
+    public float speed;
+
+    GameObject block;
     int cnt = 0;
     // Start is called before the first frame update
     void Start()
     {
         Spawn();
+        
     }
 
     // Update is called once per frame
@@ -24,12 +28,54 @@ public class GameManager : MonoBehaviour
         {
             Spawn();
         }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            if (block.transform.position.x <= -4.5f)
+            {
+                return;
+            }
+            Debug.Log("<-");
+            Vector2 cur = block.transform.position;
+            block.transform.position = cur + new Vector2(-1, 0);
+            
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            if (block.transform.position.x >= 4f)
+            {
+                return;
+            }
+            Debug.Log("->");
+            Vector2 cur = block.transform.position;
+            block.transform.position = cur + new Vector2(1, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //네모는 회전 못함
+            if(block.tag == "square")
+            {
+                Debug.Log("sqare");
+                return;
+            }
+            Debug.Log("회전");
+            block.transform.Rotate(0, 0, 90, Space.Self);
+        }
     }
 
     void Spawn()
     {
-        Rigidbody2D rigid = GetComponent<Rigidbody2D>();
-        Instantiate(j_block, spawn_point.position, spawn_point.rotation);
+        int block_rand = Random.Range(0, 3);
+        int rand = Random.Range(0, 6);        
+        block = Instantiate(blocks[block_rand], spawn_point[rand].position, spawn_point[rand].rotation);
+        Rigidbody2D rigid = block.GetComponent<Rigidbody2D>();
+        rigid.velocity = new Vector2(0 , -speed);
+        Debug.Log(block.transform.position.x);
         //rigid.velocity = new Vector2(-1, -0.1f);
     }
+
+    public void Move()
+    {
+        
+    }
+    
 }
